ef549232930b88f5f4b4966a501577e410e57cd6
Rafael Lino <rafael.veloso.lino@hotmail.com>
2023-05-26 22:49:29 -0300

    feat: setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='version-checker',
+    version='0.0.2',
+    author='Rafaellino',
+    url='https://github.com/Rafaellinos/version-checker',
+    packages=['version-checker'],
+)
